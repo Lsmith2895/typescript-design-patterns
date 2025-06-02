a93460af2498c199451946199ea3dc47c4e1ccfd
Logan Smith <lsmith2895@yahoo.com>
2025-06-02 09:33:28 -0400

    create academic example of the factory pattern

---
+++ typescript-design-petterns/src/factoryPattern/AcademicExample.ts
@@ -0,0 +1,47 @@
+abstract class Creator {
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): string {
+        const product = this.factoryMethod();
+        return `Creator: The same creators code has just worked with ${product.operation()}`;
+    }
+}
+
+class ConcreteCreator1 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct1();
+    }
+}
+
+class ConcreteCreator2 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct2();
+    }
+}
+
+interface Product {
+    operation(): string;
+}
+
+class ConcreteProduct1 implements Product {
+    public operation(): string {
+        return 'result of creating product 1';
+    }
+}
+
+class ConcreteProduct2 implements Product {
+    public operation(): string {
+        return 'result of creating product 2';
+    }
+}
+
+function clientCode(creator: Creator) {
+    console.log(creator.someOperation());
+}
+
+console.log('App: launched with concrete creator 1')
+clientCode(new ConcreteCreator1);
+console.log('')
+
+console.log('App: launched with concrete creator 2')
+clientCode(new ConcreteCreator2)
