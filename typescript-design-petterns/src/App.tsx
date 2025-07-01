0c2be224feeccb80521f86f966cad6121f70fe81
Logan Smith <lsmith2895@yahoo.com>
2025-07-01 19:33:29 -0400

    setup basic routing for use by navbar

--- typescript-design-petterns/src/App.tsx
+++ typescript-design-petterns/src/App.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { DecoratorPatternView } from './decoratorPattern/PatternView'
+import { HomePage } from './HomePage'
 import { ThemeFactoryView } from './factoryPattern/PatternView'
+import { ObserverPatternView } from './observerPattern/PatternView'
+import { StrategyPatternView } from './strategyPattern/PatternView'
+import { NavBar } from './NavBar'
 
 function App() {
 
   return (
     <>
-      <ThemeFactoryView />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/decorator-pattern' element={<DecoratorPatternView />} />
+          <Route path='/factory-pattern' element={<ThemeFactoryView />} />
+          <Route path='/observer-pattern' element={<ObserverPatternView />} />
+          <Route path='/strategy-pattern' element={ <StrategyPatternView />} />
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
