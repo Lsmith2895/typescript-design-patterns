9061f6231bd464b98bbfae078131c7e04dda3f3e
Logan Smith <lsmith2895@yahoo.com>
2025-07-01 19:38:50 -0400

    get linking working

--- typescript-design-petterns/src/NavBar.tsx
+++ typescript-design-petterns/src/NavBar.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom"
+
 function NavBar() { 
     return (
         // things we need on the nav bar:
         // Logo Icon, home, about, patterns as a drop down? 
         <nav>
-            <div> Icon </div>
-            <div> Home </div>
+            <Link to="/" >
+                Home
+            </Link>
             <div> About </div>
             {/* patterns will be its own component eventually just need to stub out the navbar for now */}
-            <div> Patterns </div> 
+            <div> Patterns </div>
          </nav>
     )
 }
